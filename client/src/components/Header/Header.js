7c0b898613700df1801bab343abc2ab9f2f669d7
ghostrib <sh4rkbot@gmail.com>
2021-01-26 07:57:13 -0600

    added header component with sign in button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import s from './header.module.scss';
 
 const Header = () => {
-  return <div>Hello</div>;
+  return (
+    <header className={s.header}>
+      <button className={s.visible}>Sign in</button>
+    </header>
+  );
 };
 
 export default Header;
