5474ca1bff6e62552e24e3a1618f2429f29849cd
ghostrib <sh4rkbot@gmail.com>
2021-01-26 07:29:52 -0600

    add components folder

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import s from './app.module.scss';
+import Header from '../Header/Header';
+import ChatBox from '../ChatBox/ChatBox';
+import SideBar from '../SideBar/Sidebar';
+import TextInput from '../TextInput/TextInput';
+import Footer from '../Footer/Footer';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header />
+      <SideBar />
+      <ChatBox />
+      <TextInput />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
