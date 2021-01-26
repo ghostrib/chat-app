9dcd9cc29e6bb5e1716242f882ea75f6c3d15bcb
ghostrib <sh4rkbot@gmail.com>
2021-01-26 08:12:13 -0600

    added sidebar layout

--- client/src/components/SideBar/Sidebar.js
+++ client/src/components/SideBar/Sidebar.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import s from './sidebar.module.scss';
 
 const SideBar = () => {
-  return <div>Hello</div>;
+  return (
+    <aside className={s.sidebar}>
+      <ul>
+        <li className={s.sidebar__list__item}>
+          <span className={s.username}>billy bob backscratch</span>
+        </li>
+        <li className={s.sidebar__list__item}>
+          <span className={s.username}>Chuck the fabulous</span>
+        </li>
+        <li className={s.sidebar__list__item}>
+          {' '}
+          <span className={s.username}>Raymond peters</span>
+        </li>
+        <li className={s.sidebar__list__item}>
+          {' '}
+          <span className={s.username}>Jason Marshals</span>
+        </li>
+        <li className={s.sidebar__list__item}>
+          {' '}
+          <span className={s.username}>Pauline Chambers</span>
+        </li>
+      </ul>
+    </aside>
+  );
 };
 
 export default SideBar;
