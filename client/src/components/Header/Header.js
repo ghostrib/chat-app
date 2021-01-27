45d181f6a5c816b9fedc99b5c2923c49a0a85487
ghostrib <sh4rkbot@gmail.com>
2021-01-26 19:53:13 -0600

    remove unused/commented out code

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -14,19 +14,6 @@ const Header = ({
 }) => {
   const buttonClass = isSignedIn ? s.hidden : s.visible;
 
-  const Component = isSignedIn ? (
-    <UserProfile
-      username={username}
-      imageUrl={imageUrl}
-      signOutWithGoogle={signOutWithGoogle}
-    />
-  ) : !isSignedIn && isSignedIn !== null ? (
-    <SignInButton
-      toggleModal={toggleModal}
-      buttonClass={buttonClass}
-    />
-  ) : null;
-
   return (
     <header className={s.header}>
       {isSignedIn ? (
@@ -54,23 +41,3 @@ Header.propTypes = {
 };
 
 export default Header;
-
-// import React, { useState } from 'react';
-// import s from './header.module.scss';
-// import PropTypes from 'prop-types';
-
-// const Header = ({ toggleModal }) => {
-//   return (
-//     <header className={s.header}>
-//       <button onClick={toggleModal} className={s.visible}>
-//         Sign in
-//       </button>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-// Header.propTypes = {
-//   toggleModal: PropTypes.func.isRequired,
-// };
--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import s from './modal.module.scss';
 
 import PropTypes from 'prop-types';
