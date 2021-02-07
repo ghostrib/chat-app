b51612ec0595fc98a0db87876ec818ccfcbe54e7
ghostrib <sh4rkbot@gmail.com>
2021-02-07 15:09:20 -0600

    setup firebase

---
+++ client/src/firebase/index.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+import { firebaseConfig } from '../config';
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+const createNewUserEntry = (state) => {
+  const { name, image, uid, isSignedIn } = state;
+  const userStatus = firebase.database().ref(`/userlist/${uid}`);
+  userStatus.set({ name, image, online: isSignedIn });
+};
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+  access_type: 'offline',
+  display: 'page',
+});
+
+provider.addScope('profile');
+
+export { provider, db, createNewUserEntry };
+
+export default firebase;
