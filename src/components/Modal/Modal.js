94595729b88a6f3ad6155b5cd52f770f4b2966b6
ghostrib <sh4rkbot@gmail.com>
2021-02-11 15:55:45 -0600

    update modal login process

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './modal.module.scss';
 
-import PropTypes from 'prop-types';
-import GoogleButton from './GoogleButton/GoogleButton';
+import LoginForm from './LoginForm/LoginForm';
 
 const Modal = ({ visible, toggleModal }) => {
   return (
@@ -10,10 +10,7 @@ const Modal = ({ visible, toggleModal }) => {
       <div className={visible ? s.visible : s.hidden}>
         <div className={s.modal}></div>
         <div className={s.modal__window}>
-          <GoogleButton />
-          <span className={s.close}>
-            <button onClick={toggleModal}>X</button>
-          </span>
+          <LoginForm toggleModal={toggleModal} />
         </div>
       </div>
     </>
