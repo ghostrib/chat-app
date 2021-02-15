b0dcf31d6236a7cb71b63790cf2a2f10e5e7d280
ghostrib <sh4rkbot@gmail.com>
2021-02-15 00:22:46 -0600

    fix: removed redundant alt description from img tag

--- src/components/SideBar/UserCard/UserCard.js
+++ src/components/SideBar/UserCard/UserCard.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import s from './usercard.module.scss';
 const UserCard = ({ name, image }) => {
   return (
     <li className={s.sidebar__list__item}>
-      <img className={s.avatar} src={image} alt="user profile picture" />
+      <img className={s.avatar} src={image} alt="user avatar" />
       <span className={s.name}>{name}</span>
     </li>
   );
