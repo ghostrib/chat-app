c4cf1c5e1405c6a44d3c43245760038c79fe2be8
ghostrib <sh4rkbot@gmail.com>
2021-03-03 04:58:01 -0600

    adjust form styles

--- src/components/Modal/Login/Login.js
+++ src/components/Modal/Login/Login.js
@@ -2,19 +2,28 @@
 import s from './login.module.scss';
 import GoogleButton from './GoogleButton';
 import FacebookButton from './FacebookButton';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import firebase from '../../../firebase';
+import utils from '../../../utils';
 // import TwitterButton from './TwitterButton';
 
 const Login = ({ select }) => {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+
+  const [ isValidEmail, setIsValidEmail ] = useState(null);
+  const [ isValidPassword, setIsValidPassword ] = useState(null);
   const [ isDisabled, setIsDisabled ] = useState(true);
+
+
+  const [ errorEmail, setErrorEmail ] = useState('');
 
   const emailLabelRef = useRef(null);
   const passwordLabelRef = useRef(null);
   const buttonRef = useRef(null);
 
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
   const handleInputChange = (e) => {
     if (e.target.name === 'email') {
@@ -22,19 +31,6 @@ const Login = ({ select }) => {
     }
     else if (e.target.name === 'password') {
       setPassword(e.target.value);
-    }
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      await firebase.auth().signInWithEmailAndPassword(email, password);
-    }
-    catch (error) {
-      console.error(error);
-    }
-    finally {
-      select.toggleModal();
     }
   };
 
@@ -56,13 +52,73 @@ const Login = ({ select }) => {
 
   const handleInputBlur = (e) => {
     const target = e.target.name;
-    const ref = target === 'email'
-      ? emailLabelRef.current
-      : target === 'password'
-      ? passwordLabelRef.current
-      : target;
-    ref.className = s.label__blur;
+    let ref;
+    if (target === 'email') {
+      ref = emailLabelRef.current;
+    }
+    else if (target === 'password') {
+      ref = passwordLabelRef.current;
+    }
+    else {
+      return null;
+    }
+
+    if (e.target.value.length === 0) {
+      ref.className = s.label__blur;
+    }
   };
+
+
+  const handleEmailValidation = async () => {
+    // const email = e.target.value;
+    if (email.length === 0) {
+      emailRef.current.className = s.email;
+      setIsValidEmail(null);
+    }
+    else if (utils.validateEmail(email)) {
+      const methods = await firebase.auth().fetchSignInMethodsForEmail(email);
+      if (methods.length) {
+        // email provider found for user email
+        emailRef.current.className = s.success;
+        setIsValidEmail(true);
+      }
+      else {
+        // no email provider found
+        emailRef.current.className = s.error;
+        setIsValidEmail(false);
+        throw new Error(`No registered user found for ${email}`);
+      }
+    }
+    else {
+      // email address is malformed
+      emailRef.current.className = s.error;
+      setIsValidEmail(false);
+      setErrorEmail(`${email} is not a valid email address`);
+      // throw new Error(`${email} is not a valid email address`);
+    }
+  };
+
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await handleEmailValidation();
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      await select.toggleModal();
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+  const setEmailClass = () => {
+    return isValidEmail === true
+      ? s.success
+      : isValidEmail === false
+      ? s.error
+      : s.email;
+  };
+
 
   const { showSignup, toggleModal } = select;
   return (
@@ -93,13 +149,14 @@ const Login = ({ select }) => {
               type="email"
               name="email"
               // placeholder="Email"
-              className={s.email}
+              className={setEmailClass()}
               onChange={handleInputChange}
-              onBlur={handleInputBlur}
-              onFocus={handleInputFocus}
+              onBlur={handleEmailValidation}
+              onFocus={e => handleInputFocus(e)}
               value={email}
               required={true}
-              autoFocus={true}
+
+              ref={emailRef}
             />
           </div>
 
@@ -120,8 +177,10 @@ const Login = ({ select }) => {
               onChange={handleInputChange}
               onBlur={handleInputBlur}
               onFocus={handleInputFocus}
+
               value={password}
               required={true}
+              ref={passwordRef}
             />
           </div>
 
