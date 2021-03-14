1c5c3b848c9341c9f1c3ce57aac49745e3351bdc
ghostrib <sh4rkbot@gmail.com>
2021-03-14 18:50:21 -0500

    ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,45 +1,20 @@
-require('dotenv').config();
-
 module.exports = {
-  apps: [
-    {
-      name: 'chat-app',
-      script: './server/index.js',
-      watch: [ 'server', 'public' ],
-      env: {
-        NODE_ENV: 'development'
-      },
-      env_development: {
-        NODE_ENV: 'development'
-      },
-      env_production: {
-        NODE_ENV: 'production'
-      }
-    }
-  ],
+  apps: [ {
+    script: './server/index.js',
+    watch: '.'
+  } ],
 
   deploy: {
-    production: {
-      user: 'deploy',
-      host: '165.232.145.102',
-      port: '22',
-      key: '~/.ssh/deploy',
-      ref: 'origin/main',
-      repo: 'git@github.com:ghostrib/chat-app.git',
-      path: '/var/www/chat-app',
-      'post-deploy':
-        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
-    },
     development: {
-      user: 'deploy',
+      user: 'gizmo',
       host: '165.232.145.102',
-      port: '22',
-      key: '~/.ssh/deploy',
       ref: 'origin/staging',
       repo: 'git@github.com:ghostrib/chat-app.git',
       path: '/var/www/chat-app',
-      'post-deploy':
-        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env development'
+      ssh_options: 'IdentityFile=~/.ssh/mattbrannon',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
     }
   }
 };
